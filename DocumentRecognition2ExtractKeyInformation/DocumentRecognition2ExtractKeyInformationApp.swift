f4ac8f5dd17479c9a43898c96cf283b7e1dd60b1
samtari1 <samtariyang@gmail.com>
2026-03-17 23:14:55 -0400

    Initial Commit

---
+++ DocumentRecognition2ExtractKeyInformation/DocumentRecognition2ExtractKeyInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentRecognition2ExtractKeyInformationApp.swift
+//  DocumentRecognition2ExtractKeyInformation
+//
+//  Created by Quanpeng Yang on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct DocumentRecognition2ExtractKeyInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DocumentRecognition2ExtractKeyInformationTests/DocumentRecognition2ExtractKeyInformationTests.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentRecognition2ExtractKeyInformationTests.swift
+//  DocumentRecognition2ExtractKeyInformationTests
+//
+//  Created by Quanpeng Yang on 3/17/26.
+//
+
+import Testing
+@testable import DocumentRecognition2ExtractKeyInformation
+
+struct DocumentRecognition2ExtractKeyInformationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
